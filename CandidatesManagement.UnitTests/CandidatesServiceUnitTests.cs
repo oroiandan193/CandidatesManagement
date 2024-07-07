ebc605fde46682aec8a0312286d5befae8a8d584
danoroian-ithuba <danoroian@ithuba.ro>
2024-07-07 11:34:52 +0300

    Fix unit tests

--- CandidatesManagement.UnitTests/CandidatesServiceUnitTests.cs
+++ CandidatesManagement.UnitTests/CandidatesServiceUnitTests.cs
@@ -5,6 +5,7 @@ using CandidatesManagement.Core.Entities;
 using CandidatesManagement.Core.Repositories;
 using CandidatesManagement.Application.Services;
 using CandidatesManagement.Application.Contracts.Dtos;
+using CandidatesManagement.Core.ValueObjects;
 
 namespace CandidatesManagement.UnitTests;
 
@@ -31,33 +32,66 @@ public class CandidatesServiceTests
             // Other properties as needed
         };
 
-        mockRepository.Setup(r => r.FindByEmailAddressAsync(dto.EmailAddress))
-                      .ReturnsAsync((JobCandidate)null);
-
         // Act  & Assert
         Assert.ThrowsAsync<ArgumentException>(async () => await candidatesService.UpsertJobCandidateAsync(dto));
     }
 
     [Test]
-    public async Task UpsertJobCandidateAsync_UpdateExistingCandidate_CandidateIsUpdated()
+    public async Task UpsertJobCandidateAsync_CandidateDoesntExist_JobCandidateIsInserted()
     {
         // Arrange
-        var candidateBuilder = new JobCandidateBuilder();
+        var builder = new UpsertJobCandidateDtoBuilder()
+            .WithFirstName("John")
+            .WithLastName("Doe")
+            .WithEmailAddress("john.doe@example.com")
+            .WithPhoneNumber("123-456-7890")
+            .WithIntervalStart(TimeSpan.FromHours(9))
+            .WithIntervalEnd(TimeSpan.FromHours(17))
+            .WithLinkedInProfile("https://linkedin.com/in/johndoe")
+            .WithGitHubProfile("https://github.com/johndoe")
+            .WithComment("This is a test comment");
 
-        var existingCandidate = candidateBuilder.Build();
+        var dto = builder.Build();
 
-        var dto = candidateBuilder.BuildDto();
+        var candidateEntity = dto.ToEntity();
+
+        // Act
+        await candidatesService.UpsertJobCandidateAsync(dto);
+
+        // Assert
+        mockRepository.Verify(r => r.UpdateJobCandidateAsync(candidateEntity), Times.Never);
+        mockRepository.Verify(r => r.InsertJobCandidateAsync(It.IsAny<JobCandidate>()), Times.Once);
+    }
+
+    [Test]
+    public async Task UpsertJobCandidateAsync_UpdateExistingCandidate_JobCandidateIsUpdated()
+    {
+        // Arrange
+        var builder = new UpsertJobCandidateDtoBuilder()
+            .WithFirstName("John")
+            .WithLastName("Doe")
+            .WithEmailAddress("john.doe@example.com")
+            .WithPhoneNumber("123-456-7890")
+            .WithIntervalStart(TimeSpan.FromHours(9))
+            .WithIntervalEnd(TimeSpan.FromHours(17))
+            .WithLinkedInProfile("https://linkedin.com/in/johndoe")
+            .WithGitHubProfile("https://github.com/johndoe")
+            .WithComment("This is a test comment");
+
+        var dto = builder.Build();
+
+        var candidateEntity = dto.ToEntity();
 
         dto.PhoneNumber = "+40123456789";
 
-        mockRepository.Setup(r => r.FindByEmailAddressAsync(dto.EmailAddress))
-                      .ReturnsAsync(existingCandidate);
+        mockRepository.Setup(r => r.FindByEmailAddressAsync(candidateEntity.EmailAddress))
+                      .ReturnsAsync(candidateEntity);
 
         // Act
         await candidatesService.UpsertJobCandidateAsync(dto);
 
         // Assert
         mockRepository.Verify(r => r.InsertJobCandidateAsync(It.IsAny<JobCandidate>()), Times.Never);
-        mockRepository.Verify(r => r.UpdateJobCandidateAsync(existingCandidate), Times.Once);
+        mockRepository.Verify(r => r.UpdateJobCandidateAsync(candidateEntity), Times.Once);
     }
 }
---
+++ CandidatesManagement.UnitTests/UpsertJobCandidateDtoBuilder.cs
@@ -0,0 +1,101 @@
+﻿using CandidatesManagement.Application.Contracts.Dtos;
+
+namespace CandidatesManagement.UnitTests;
+
+public class UpsertJobCandidateDtoBuilder
+{
+    private string _firstName;
+    private string _lastName;
+    private string _emailAddress;
+    private string _phoneNumber;
+    private TimeSpan? _intervalStart;
+    private TimeSpan? _intervalEnd;
+    private string _linkedInProfile;
+    private string _gitHubProfile;
+    private string _comment;
+
+    public UpsertJobCandidateDtoBuilder()
+    {
+        // Set default values if any
+        _firstName = string.Empty;
+        _lastName = string.Empty;
+        _emailAddress = string.Empty;
+        _phoneNumber = null;
+        _intervalStart = null;
+        _intervalEnd = null;
+        _linkedInProfile = string.Empty;
+        _gitHubProfile = string.Empty;
+        _comment = string.Empty;
+    }
+
+    public UpsertJobCandidateDtoBuilder WithFirstName(string firstName)
+    {
+        _firstName = firstName;
+        return this;
+    }
+
+    public UpsertJobCandidateDtoBuilder WithLastName(string lastName)
+    {
+        _lastName = lastName;
+        return this;
+    }
+
+    public UpsertJobCandidateDtoBuilder WithEmailAddress(string emailAddress)
+    {
+        _emailAddress = emailAddress;
+        return this;
+    }
+
+    public UpsertJobCandidateDtoBuilder WithPhoneNumber(string phoneNumber)
+    {
+        _phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public UpsertJobCandidateDtoBuilder WithIntervalStart(TimeSpan? intervalStart)
+    {
+        _intervalStart = intervalStart;
+        return this;
+    }
+
+    public UpsertJobCandidateDtoBuilder WithIntervalEnd(TimeSpan? intervalEnd)
+    {
+        _intervalEnd = intervalEnd;
+        return this;
+    }
+
+    public UpsertJobCandidateDtoBuilder WithLinkedInProfile(string linkedInProfile)
+    {
+        _linkedInProfile = linkedInProfile;
+        return this;
+    }
+
+    public UpsertJobCandidateDtoBuilder WithGitHubProfile(string gitHubProfile)
+    {
+        _gitHubProfile = gitHubProfile;
+        return this;
+    }
+
+    public UpsertJobCandidateDtoBuilder WithComment(string comment)
+    {
+        _comment = comment;
+        return this;
+    }
+
+    public UpsertJobCandidateDto Build()
+    {
+        return new UpsertJobCandidateDto
+        {
+            FirstName = _firstName,
+            LastName = _lastName,
+            EmailAddress = _emailAddress,
+            PhoneNumber = _phoneNumber,
+            IntervalStart = _intervalStart,
+            IntervalEnd = _intervalEnd,
+            LinkedInProfile = _linkedInProfile,
+            GitHubProfile = _gitHubProfile,
+            Comment = _comment
+        };
+    }
+}
+
